666d4e8266f9975e1ae915fa449b83fb35552645
iDeimos <32171192+iDeimos@users.noreply.github.com>
2021-02-18 20:16:45 +0100

    Create DiceArduinoOut.py
    # How to use:
    # 1. LISTNAME should be your list of rolled dice numbers
    # 2. arduino refers to the setup variable attached to your serial communication set up
    #       EXAMPLE:  port = 'COM3'
                      baudrate = 9600
                      timeout = 1
                      arduino = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
    # 3. This script relies upon you setting up serial recieving bytes on your arduino with the digits 1-6.
    # A very basic setup of this can be found in arduinoDICE.ino
    # 4. Enjoy :)

---
+++ DiceArduinoOut.py
@@ -0,0 +1,21 @@
+
+
+
+def checklistval():  # Logic to turn on specified LEDs based on list input
+    for num in range(1, 7):  # Check for the numbers 1-6
+        if num in LISTNAME:  # Is the integer NUM in the list of rolls?
+            if LISTNAME.count(num) > 1:  # Are there duplicates of the value?
+                x_rolls = LISTNAME.count(num)
+                print('There are ' + str(x_rolls) + ' duplicates of the number ' + str(num))    # Control string
+
+                while x_rolls:  # Prints duplicates on the arduino LED setup
+                    values = bytearray(str(num).encode())
+                    time.sleep(1)
+                    arduino.write(values)
+                    time.sleep(1)
+                    x_rolls = x_rolls - 1
+            else:   # Prints non-duplicates on the arduino LED setup
+                values = bytearray(str(num).encode())
+                time.sleep(1)
+                arduino.write(values)
+                time.sleep(1)
